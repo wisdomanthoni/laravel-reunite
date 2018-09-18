8643ed076dbff5de14d393f74c0a8587eaca58dc
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-18 17:16:32 +0100

    changes made

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/film', 'HomeController@index')->name('home');
