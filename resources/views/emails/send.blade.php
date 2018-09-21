8bddca3d64688eff34220ebc15375fcf4397d4ec
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-21 16:16:54 +0100

    fix issue

--- resources/views/emails/send.blade.php
+++ resources/views/emails/send.blade.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,29 +12,30 @@
         
         *{
             font-family: 'Open Sans';
+            color: #000;
         }
     </style>
-    <div style="padding: 43px 53px; background-color: white; width:632px;">
-        <img src="https://firebasestorage.googleapis.com/v0/b/trend-76131.appspot.com/o/images%2Flogo.svg?alt=media&token=4c82938d-d846-4f84-b6e2-fa281ff0436f" alt="" style="width:115px; height: 115px;">
-        <h2 style="line-height:29px; font-size: 25px; ">
+    <div style="padding: 43px 53px; background-color: white; width:632px; margin:auto">
+        <img src="https://firebasestorage.googleapis.com/v0/b/trend-76131.appspot.com/o/images%2Flogo.png?alt=media&token=b4b5bf23-cd8e-418a-b90d-8461355f9e37" alt="" style="width:115px; height: 115px;">
+        <h2 style="line-height:29px; font-size: 18px; color: black !important;">
             Dear {{$firstname}}
         </h1>
-        <p style="max-width: 418px; line-height:23px; font-size: 18px;">
+        <p style="max-width: 418px; line-height:23px; font-size: 12px; color: #000;">
             This your ticket confirmation for GDG Devfest
             South-South Nigeria 2018
         </p>
-        <h3 style="line-height:23px; font-size: 20px;">
+        <h3 style="line-height:23px; font-size: 15px; color: #000;">
             Your Ticket
         </h3>
-        <p style="line-height:23px; font-size: 18px;">
+        <p style="line-height:23px; font-size: 12px; color: #000;">
             1 x {{$type}}
         </p>
 
         <div style="background-color: #F5F5F5; padding: 31px 39px;">
-            <h4>
+            <h4 style="font-size: 12px;">
                 Order Information
             </h4>
-            <p style="line-height:23px; font-size: 18px;">Order Id: {{$id}}</p>
+            <p style="line-height:23px; font-size: 12px;">Order Id: {{$id}}</p>
             <table style="width:500px;"  cellspacing="0">
                 <tbody>
                     <tr>
@@ -48,22 +50,22 @@
             </table>
         </div>
         <center>
-            <p style="width: 430px; font-size: 15px; color: #52555A;">
-                If you are not happy with your order you can cancel your ticket, send a mail to hello@devfesss.tech with reasons.
+            <p style="width: 430px; font-size: 10px; color: #52555A;">
+               If you are not happy with your order you can cancel your ticket, send a mail to hello@devfesss.tech with reasons.
             </p>
         </center>
         <!-- <button style="background-color: #F76F67; border: none; line-height: 29px; font-weight: bold; font-size:25px; color: white; width:100%; height:57px;">
             CANCEL TICKET
         </button> -->
         <center>
-            <p style="width:430px; color: #1481C5; font-size: 15px">Note: The deadline to request refunds is November 1st, 2018.
+            <p style="width:430px; color: #1481C5; font-size: 10px">Note: The deadline to request refunds is November 1st, 2018.
                 After this date,
                 all sales are final.
             </p>
         </center>
-        <div style="margin-bottom: -20px">
-            <p>Best Regards</p>
-            <h3 style="line-height:23px; font-size: 20px;">GDG Uyo Team</h3>
+        <div style="margin-bottom: -20px; color: #000;">
+            <p style="font-size: 12px">Best Regards</p>
+            <h3 style="line-height:23px; font-size: 18px;">GDG Uyo Team</h3>
         </div>
     </div>
     
