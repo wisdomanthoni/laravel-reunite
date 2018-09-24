78aac09cbfc1ed211c6e48e422a72258a26a2850
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-24 18:31:46 +0100

    fix issue

--- database/factories/CouponFactory.php
+++ database/factories/CouponFactory.php
@@ -2,10 +2,10 @@
 
 use Faker\Generator as Faker;
 use App\Token;
+use App\Coupon;
 
 $factory->define(Coupon::class, function (Faker $faker) {
     $code = new Token;
-
     return [
         'coupon' => $code->limit(6),
     ];
